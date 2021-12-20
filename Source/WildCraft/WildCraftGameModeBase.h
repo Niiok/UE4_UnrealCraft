2e4ff28a9af70b8c6f1a09d33b4aa63a72fc537a
Niiok <play.niiok@gmail.com>
2021-12-20 22:01:04 +0900

    project generate

---
+++ Source/WildCraft/WildCraftGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WildCraftGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WILDCRAFT_API AWildCraftGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
