bf1c82913fcb813a5555a8fedca90367b71ec688
Niiok <play.niiok@gmail.com>
2021-12-26 23:28:33 +0900

    save data structure scheming

---
+++ Source/WildCraft/SaveGame/WCSaveGame_World.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "WCSaveGame_World.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WILDCRAFT_API UWCSaveGame_World : public USaveGame
+{
+	GENERATED_BODY()
+	
+		// creator ID
+		// owner ID
+
+		// terrrain
+
+		// time
+
+		// special dropped items
+		// special alivings
+			// npc
+			// named monster
+
+		// story (quest)
+};
