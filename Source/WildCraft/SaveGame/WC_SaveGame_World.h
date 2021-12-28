91f6e200c2d6faebcc783ed6f2496d2696d892e6
Niiok <play.niiok@gmail.com>
2021-12-28 20:39:35 +0900

    resource import & etc
    - name changed
    - character init
    - input setting change

---
+++ Source/WildCraft/SaveGame/WC_SaveGame_World.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "WC_SaveGame_World.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WILDCRAFT_API UWC_SaveGame_World : public USaveGame
+{
+	GENERATED_BODY()
+	
+		// creator ID
+		// owner ID
+
+		// terrrain
+
+		// time
+
+		// special dropped items
+		// special alivings
+			// npc
+			// named monster
+
+		// story (quest)
+};
