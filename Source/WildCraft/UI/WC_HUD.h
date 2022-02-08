0005612b760bffe83341f50dd620862177c3b8c1
Niiok <play.niiok@gmail.com>
2022-02-08 23:56:23 +0900

    hud?

---
+++ Source/WildCraft/UI/WC_HUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "WC_HUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WILDCRAFT_API AWC_HUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
